dbaf1184a8d14d69acbc6247e20ae8a7c0aaa1e5
Giousa <65489469@qq.com>
2017-06-28 16:21:40 +0800

    first commit

---
+++ app/src/main/java/com/zmm/listviewpaging/MainActivity.java
@@ -0,0 +1,59 @@
+package com.zmm.listviewpaging;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.list_view)
+    ListView mListView;
+
+    private List<String> mDeviceNames;
+    private MyListAdapter mListAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        init();
+    }
+
+    private void init() {
+        mDeviceNames = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            mDeviceNames.add("TWS01211313_" + i);
+        }
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        mListAdapter = new MyListAdapter(getApplicationContext(),mDeviceNames);
+        mListView.setAdapter(mListAdapter);
+    }
+
+    @OnClick({R.id.btn_pre, R.id.btn_next})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_pre:
+                mListAdapter.turnToPre();
+                break;
+            case R.id.btn_next:
+                mListAdapter.turnToNext();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/zmm/listviewpaging/MyListAdapter.java
@@ -0,0 +1,128 @@
+package com.zmm.listviewpaging;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Description:
+ * Author:zhangmengmeng
+ * Date:2017/6/28
+ * Time:下午4:05
+ */
+
+public class MyListAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<String> mDeviceNames;
+    private int mItemCount = 3;
+    private int mCurrentPage = 0;
+    private int mSize = 0;
+
+    public MyListAdapter(Context context, List<String> deviceNames) {
+        mContext = context;
+        mDeviceNames = deviceNames;
+        if(deviceNames != null && deviceNames.size() > 0){
+            mSize = mDeviceNames.size();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        int i = mDeviceNames.size() % mItemCount;
+        if(i == 0){
+            return mItemCount;
+        }else {
+            return (isLastOne()) ? i : mItemCount;
+        }
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_device_register, parent, false);
+            holder = new ViewHolder();
+            holder.mDeviceId = convertView.findViewById(R.id.tv_device_id);
+            holder.mDeviceItem = convertView.findViewById(R.id.ll_device_item);
+            convertView.setTag(holder);
+
+        } else {
+
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        final int currentPosition = position + mCurrentPage * mItemCount;
+        if(currentPosition < mDeviceNames.size()){
+            holder.mDeviceId.setText(mDeviceNames.get(currentPosition));
+            holder.mDeviceItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("当前选中的position = "+currentPosition);
+                }
+            });
+        }
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        LinearLayout mDeviceItem;
+        TextView mDeviceId;
+    }
+
+    private boolean isFirstOne() {
+        return this.mCurrentPage == 0;
+    }
+
+    private boolean isLastOne() {
+        int i = mDeviceNames.size() % mItemCount;
+        if(i == 0){
+            return this.mCurrentPage == mSize/mItemCount - 1;
+        }else {
+            return this.mCurrentPage == mSize/mItemCount;
+        }
+    }
+
+    public void turnToPre() {
+
+        if (!isFirstOne()) {
+            mCurrentPage--;
+            notifyDataSetChanged();
+        } else {
+            System.out.println("已经是第一页");
+            Toast.makeText(mContext,"已经是第一页",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void turnToNext() {
+
+        if (!isLastOne()) {
+            mCurrentPage++;
+            notifyDataSetChanged();
+        } else {
+            System.out.println("已经是最后一页");
+            Toast.makeText(mContext,"已经是最后一页",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
